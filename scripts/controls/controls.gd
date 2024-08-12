98367002d686a25ab7cbc8cde357cb1d48223578
Cole Landers <jclanders@protonmail.com>
2024-08-12 13:15:14 -0500

    Remove Lunge/Move Input Code
    The lunge code previously added was just PoC and I wanted to tidy up some folders

---
+++ scripts/controls/controls.gd
@@ -0,0 +1,18 @@
+extends Object
+class_name Controls
+class Player:
+	extends Object
+	static func move_node2D(node: Node2D, speed: int):
+		var x_direction = Input.get_axis("left", "right")
+		var y_direction = Input.get_axis("up", "down")
+		if x_direction:
+			node.velocity.x = x_direction * speed
+		else:
+			node.velocity.x = move_toward(node.velocity.x, 0, speed)
+			
+		if y_direction:
+			node.velocity.y = y_direction * speed
+		else:
+			node.velocity.y = move_toward(node.velocity.y, 0, speed)
+
+		node.move_and_slide()
